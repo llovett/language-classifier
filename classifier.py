52f70e0aa4b4486f626919e96f1b363b29f9e9fc
llovett <llovett@oberlin.edu>
2013-04-17 21:29:03 -0400

    calculating log probabilities of characters given a language, language given a document

--- classifier.py
+++ classifier.py
@@ -7,10 +7,13 @@
 # Last Modified: Wed Apr 17 20:38:42 EDT 2013
 
 from sys import argv
+from math import log
 import os
 from pprint import pprint
 
-# Information we need for the classifier
+# Languages
+LANGUAGES = ('English','Spanish','Japanese')
+# Character information for languages
 DB = {
     'English':
         {
@@ -31,12 +34,28 @@ DB = {
         'all_chars':0
         }
 }
+# Count of all documents examined
 All_docs_count = 0
+# Language document probabilities
+P_eng = 0
+P_spn = 0
+P_jap = 0
+
+def char_index(c):
+    return ord(c.lower())-ord('a')
+
+def char_prob(char, lang):
+    '''Returns the log probability of the <char> appearing in the <lang>'''
+    return log(float(DB[lang][char_index(char)])) - log(float(DB[lang]['all_chars']))
+
+def lang_prob(document, lang):
+    '''Returns the log probability of the <document> being in <lang>'''
+    return sum( (char_prob(c) for c in document) )
 
 def language(document):
-    '''Returns the most likely language for the given <document>.'''
+    '''Returns the most likely language for the given <document>'''
+    return max((lang_prob(document, lang) for lang in LANGUAGES))
     
-
 def main():
     if len(argv) < 3:
         print "USAGE: python classifier.py <training set dir> <test set dir>"
@@ -45,12 +64,9 @@ def main():
     traindir = argv[1]
     testdir = argv[2]
 
-    # Convert character to array index
-    index = lambda char:ord(char.lower())-ord('a')
-
     # Go through each training set file and build knowledge base
-    global All_docs_count
-    for language in ('English','Japanese','Spanish'):
+    global All_docs_count, P_eng, P_spn, P_jap
+    for language in LANGUAGES:
         dirname = os.path.join(traindir,language)
         for ifname in os.listdir(dirname):
             # Increment count for document in current language
@@ -63,9 +79,14 @@ def main():
                 for char in contents:
                     if char.isalpha():
                         # Increment single-character count
-                        lang_db['char_counts'][index(char)] += 1
+                        lang_db['char_counts'][char_index(char)] += 1
                         # Increment all character count for this language
                         lang_db['all_chars'] += 1
+
+    # Set other data
+    P_eng = log(DB['English']['document_count']) - log(float(All_docs_count))
+    P_spn = log(DB['Spanish']['document_count']) - log(float(All_docs_count))
+    P_jap = log(DB['Japanese']['document_count']) - log(float(All_docs_count))
 
     pprint(DB)
 
